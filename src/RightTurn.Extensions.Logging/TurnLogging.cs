4d3a8cf78fb02746549d818c50ff5f5f6e3a9595
Matt Janda <matt.janda.kingston@gmail.com>
2021-01-24 22:48:25 +0000

    feat: Add turn logging extensions

---
+++ src/RightTurn.Extensions.Logging/TurnLogging.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace RightTurn.Extensions.Logging
+{
+    internal class TurnLogging : ITurnLogging
+    {
+        public void AddLogging(ITurn turn)
+        {
+            if (turn.Directions.Have<Action<ILoggingBuilder>>(out var loggingBuilder))
+                turn.Directions.Get<IServiceCollection>().AddLogging(loggingBuilder);
+        }
+    }
+}
---
+++ src/RightTurn.Extensions.Logging/TurnLoggingExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace RightTurn.Extensions.Logging
+{
+    public static class TurnLoggingExtensions
+    {
+        public static ITurn WithLogging(this ITurn turn)
+        {
+            turn.Directions.Add<ITurnLogging>(new TurnLogging());
+            return turn;
+        }
+
+        public static ITurn WithLogging(this ITurn turn, Action<ILoggingBuilder> logging)
+        {
+            turn.Directions.Add(logging);
+            return turn;
+        }
+
+        public static ITurn WithUnhandledExceptionLogging(this ITurn turn)
+        {
+            turn.Directions.Add<Func<Exception, ITurn, int>>((ex, turn) =>
+            {
+                if (!turn.Directions.HaveService<ILogger<Turn>>(out var logger))
+                    throw ex;
+
+                logger.LogCritical(ex.Message, ex);
+                return ex.HResult;
+            });
+
+            return turn;
+        }
+    }
+}
