f2d01d014924032b3b35b9dea6e2a253e14d0ae7
Matt Janda <matt.janda.kingston@gmail.com>
2021-03-27 00:51:54 +0000

    feat: Improve logging extensions

--- src/RightTurn.Extensions.Logging/TurnLogging.cs
+++ src/RightTurn.Extensions.Logging/TurnLogging.cs
@@ -9,6 +9,13 @@ namespace RightTurn.Extensions.Logging
     /// </summary>
     internal class TurnLogging : ITurnLogging
     {
+        readonly Action<ILoggingBuilder> _logging;
+
+        public TurnLogging(Action<ILoggingBuilder> logging)
+        {
+            _logging = logging;
+        }
+
         /// <summary>
         /// This method is called from Turn.Take to add logging with logging builder. 
         /// You must add Action<ILoggingBuilder> to direction container using WithLogging extensions.
@@ -16,8 +23,7 @@ namespace RightTurn.Extensions.Logging
         /// <param name="turn">Turn instance</param>
         public void AddLogging(ITurn turn)
         {
-            if (turn.Directions.Have<Action<ILoggingBuilder>>(out var loggingBuilder))
-                turn.Directions.Get<IServiceCollection>().AddLogging(loggingBuilder);
+            turn.Directions.ServiceCollection().AddLogging(_logging);
         }
     }
 }
